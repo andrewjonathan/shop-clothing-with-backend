3e61df8c1e83c7069833bd70dab7ca8f0b7727fa
Andrew Jonathan <andrew@mitrafin.co.id>
2020-04-29 23:40:26 +0700

    implement code that pulls in our shop data from firestore and stores it into the shop from our shop page. Also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
     measurementId: "G-W8QSZHCX79"
   };
 
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     // If the userAuth is null return nothing
     if (!userAuth) return;
@@ -42,14 +44,42 @@ const config = {
     return userRef;
   };
 
-  firebase.initializeApp(config);
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+      const collectionRef = firestore.collection(collectionKey);
+
+      const batch = firestore.batch();
+      objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+      });
+
+      return await batch.commit();
+  }
+
+
+  export const convertCollectionsSnapshotToMap = (collectionsSnapshot) => {
+        const transformedCollection = collectionsSnapshot.docs.map(docSnapshot => {
+            const { title, items } = docSnapshot.data();
+
+            return {
+                routeName: encodeURI(title.toLowerCase()),
+                id: docSnapshot.id,
+                title,
+                items
+            }
+        });
+
+        return transformedCollection.reduce((accumulator, collection) => {
+            accumulator[collection.title.toLowerCase()] = collection;
+            return accumulator;
+        }, {});
+    };
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
 
   const provider = new firebase.auth.GoogleAuthProvider();
-
   // Signin with selected account on Google
   provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
